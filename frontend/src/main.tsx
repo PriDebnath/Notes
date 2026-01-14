2e0a0be95b151d517e5a1531ad7492f210fa3467
Pritam <debnathpritam0802@gmail.com>
2026-01-14 16:44:17 +0530

    Used providers

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,12 @@ import App from './App.tsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { TanstackRouterProvider } from '@/provider/tanstack-router.provider.tsx'
+import { TanstackQueryClientProvider } from '@/provider/query-client.provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <TanstackRouterProvider />
+    <TanstackQueryClientProvider>
+      <TanstackRouterProvider />
+    </TanstackQueryClientProvider>
   </StrictMode>,
 )
--- frontend/src/routes/$quoteId.tsx
+++ frontend/src/routes/$quoteId.tsx
@@ -1,9 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useGetQuoteDetails } from '@/hook/get-quote-details.hook'
 
 export const Route = createFileRoute('/$quoteId')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/$quoteId"!</div>
+  const {quoteId} = Route.useParams()
+
+  const {data: quote, isLoading, error} = useGetQuoteDetails(Number(quoteId))
+  
+  return <div>
+    {quote?.text}
+    {isLoading && <div>Loading...</div>}
+    {error && <div>Error: {error}</div>}
+  </div>
 }

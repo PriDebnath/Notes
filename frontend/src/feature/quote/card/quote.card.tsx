d6a65206d93d27a0a15144f27c9f395ea52b310f
Pritam <debnathpritam0802@gmail.com>
2026-01-14 17:01:54 +0530

    Link quote to new edit page

--- frontend/src/feature/quote/card/quote.card.tsx
+++ frontend/src/feature/quote/card/quote.card.tsx
@@ -3,7 +3,8 @@ import { Button } from "@/components/ui/button";
 import { sanitizeHTML } from "@/helper/sanitize-html";
 import { cn } from "@/lib/utils";
 import type { Quote, QuoteDetails } from "@/model/quote.model";
-import { Check, Copy, PenIcon, Trash } from "lucide-react";
+import { Link } from "@tanstack/react-router";
+import { Check, Copy, Maximize2, PenIcon, Trash } from "lucide-react";
 import { useState } from "react";
 
 interface Props {
@@ -24,17 +25,22 @@ const QuoteCard = (props: Props) => {
         }, 500);
     }
 
-    return (<div className={
-        cn(
-            "bg-rose-50 rounded-xl p-4 flex flex-col justify-between items-start gap-2",
-            "shadow-[0_2px_12px_rgba(0,0,0,0.1)]",
-            "hover:shadow-[0_4px_12px_rgba(0,0,0,0.2)]",
-            "transition-shadow duration-200",
-            "border-primary/30"
-        )
-    }>
+    return (
+        <Link to={"/$quoteId"}
+            params={
+                { quoteId: quote.id?.toString()! }
+            }>
 
-        <div className="
+            <div className={
+                cn(
+                    "bg-rose-50 rounded-xl p-4 flex flex-col justify-between items-start gap-2",
+                    "shadow-[0_2px_12px_rgba(0,0,0,0.1)]",
+                    "hover:shadow-[0_4px_12px_rgba(0,0,0,0.2)]",
+                    "transition-shadow duration-200",
+                    "border-primary/30 relative"
+                )
+            }>
+                <div className="
                 tiptap
                 prose
                 prose-sm 
@@ -44,57 +50,58 @@ const QuoteCard = (props: Props) => {
                 m-5 
                 focus:outline-none
             ">   {/* IMPORTANT */}
-            <div dangerouslySetInnerHTML={{ __html: sanitizeHTML(quote.text) }}></div>
-        </div>
-        <p className="text-base w-full text-right">-- Pritam</p>
-
-        <div className="flex w-full items-end justify-between gap-2">
-            {quote.tags && quote.tags.length > 0 ? (
-                <div className="flex flex-wrap gap-2">
-                    {quote.tags.map((tag, index) => (
-                        <Badge
-                        variant={'outline'}
-                            className=" bg-primary/10 border-primary/30 text-primary/90"
-                            key={tag.id}
-                        >
-                            #{tag.name}
-                        </Badge>
-                    ))}
+                    <div dangerouslySetInnerHTML={{ __html: sanitizeHTML(quote.text) }}></div>
                 </div>
-            ) : (<span></span>)}
+                <p className="text-base w-full text-right">-- Pritam</p>
 
-            <div className="flex items-center gap-2 ">
-                <Button
-                    className="hover:text-green-600 border-primary/30"
-                    variant={"outline"}
-                    onClick={() => onCopy(quote.text)}
-                    aria-label="Copy quote"
-                    size={"sm"}
-                >
-                    {copying ? <Check className="text-green-500" /> : <Copy />}
-                </Button>
-                <Button
-                    className="hover:text-yellow-600 border-primary/30"
-                    variant={"outline"}
-                    onClick={() => onEdit(quote)}
-                    aria-label="Edit quote"
-                    size={"sm"}
-                >
-                    <PenIcon />
-                </Button>
+                <div className="flex w-full items-end justify-between gap-2">
+                    {quote.tags && quote.tags.length > 0 ? (
+                        <div className="flex flex-wrap gap-2">
+                            {quote.tags.map((tag, index) => (
+                                <Badge
+                                    variant={'outline'}
+                                    className=" bg-primary/10 border-primary/30 text-primary/90"
+                                    key={tag.id}
+                                >
+                                    #{tag.name}
+                                </Badge>
+                            ))}
+                        </div>
+                    ) : (<span></span>)}
 
-                <Button
-                    className=" hover:text-destructive border-primary/30"
-                    variant={"outline"}
-                    onClick={() => onDelete(quote)}
-                    aria-label="Delete quote"
-                    size={"sm"}
-                >
-                    {<Trash />}
-                </Button>
+                    <div className="flex items-center gap-2 ">
+                        <Button
+                            className="hover:text-green-600 border-primary/30"
+                            variant={"outline"}
+                            onClick={() => onCopy(quote.text)}
+                            aria-label="Copy quote"
+                            size={"sm"}
+                        >
+                            {copying ? <Check className="text-green-500" /> : <Copy />}
+                        </Button>
+                        <Button
+                            className="hover:text-yellow-600 border-primary/30"
+                            variant={"outline"}
+                            onClick={() => onEdit(quote)}
+                            aria-label="Edit quote"
+                            size={"sm"}
+                        >
+                            <PenIcon />
+                        </Button>
+
+                        <Button
+                            className=" hover:text-destructive border-primary/30"
+                            variant={"outline"}
+                            onClick={() => onDelete(quote)}
+                            aria-label="Delete quote"
+                            size={"sm"}
+                        >
+                            {<Trash />}
+                        </Button>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
+        </Link>
     )
 }
 
--- frontend/src/routes/$quoteId.tsx
+++ frontend/src/routes/$quoteId.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useGetQuoteDetails } from '@/hook/get-quote-details.hook'
+import Tiptap from '@/components/common/tiptap-customized'
 
 export const Route = createFileRoute('/$quoteId')({
   component: RouteComponent,
@@ -12,6 +13,7 @@ function RouteComponent() {
   
   return <div>
     {quote?.text}
+    <Tiptap value={quote?.text} onValueUpdate={()=>{}} />
     {isLoading && <div>Loading...</div>}
     {error && <div>Error: {error}</div>}
   </div>

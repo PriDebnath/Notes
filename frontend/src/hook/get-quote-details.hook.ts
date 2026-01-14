16aa5fabb8c2fb09efbf2d9c84a6991062076824
Pritam <debnathpritam0802@gmail.com>
2026-01-14 16:44:02 +0530

    - tanstack query hook to fetch quote details

---
+++ frontend/src/hook/get-quote-details.hook.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query"
+import { getQuoteDetails } from "@/db/quote_tags.db"
+
+export const useGetQuoteDetails = (quoteId: number) => {
+    const { data, isLoading, error} = useQuery({
+    queryKey: ['quoteDetails', quoteId],
+    queryFn: () => getQuoteDetails(quoteId),
+  })
+  const errorString = error?.message
+  return { data, isLoading , error: errorString}
+}

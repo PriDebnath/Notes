d86818f7cf4bb451b4020c8c8b47fceb7e9877ce
Pritam <debnathpritam0802@gmail.com>
2026-01-14 15:08:25 +0530

    added router plugin to vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,13 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import routerPlugin  from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/Quotes-Keeper-3/", // 👈 EXACT repo name (for github pages)
   plugins: [
+    routerPlugin(), // Generate the router code
     react(
       {
         babel: {

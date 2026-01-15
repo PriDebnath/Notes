3c7bb37dc50f3c8bef7ca9004e22ca13cca02798
Pritam <debnathpritam0802@gmail.com>
2026-01-15 10:36:46 +0530

    moved quote page

---
+++ frontend/src/page/quote/quote.page.tsx
@@ -0,0 +1,90 @@
+
+
+import { Route } from '@/routes/$quoteId'
+import { ArrowLeftIcon } from 'lucide-react'
+import { Label } from "@/components/ui/label"
+import { Badge } from "@/components/ui/badge"
+import { Button } from '@/components/ui/button'
+import TagField from "@/feature/quote/form-field/tag"
+import { Separator } from "@/components/ui/separator"
+import { AnimatePresence, motion } from 'framer-motion'
+import Tiptap from '@/components/common/tiptap-customized'
+import { createFileRoute, Link } from '@tanstack/react-router'
+import { useGetQuoteDetails } from '@/hook/get-quote-details.hook'
+
+
+
+export function QuotePage() {
+  const { quoteId } = Route.useParams()
+
+  const { data: quote, isLoading, error } = useGetQuoteDetails(Number(quoteId))
+
+const onTagChoose = (tag: string) => {
+}
+
+const onValueUpdate = (text: string) => {
+}
+
+  return (<div className='w-full overflow-hidden'>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={quoteId}
+        initial={{ x: 40, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: -40, opacity: 0 }}
+        transition={{ duration: 0.4, ease: "easeOut" }}
+        className="w-full p-4"
+      >
+        <div className='flex flex-row  justify-between'>
+          <Link to="/" className='flex items-center gap-2 mb-4'>
+            <Button variant="outline" size="icon">
+              <ArrowLeftIcon />
+            </Button>
+          </Link>
+
+
+          <Button type="submit" onClick={() => { }}>
+            {/* {mode == "add" ? "Add Quote" : "Update Quote"} */}
+            Save
+          </Button>
+        </div>
+
+        {isLoading && <div>Loading...</div>}
+        {error && <div>Error: {error}</div>}
+        
+          <div className="grid gap-4">
+            <div className="grid gap-3">
+              <Label htmlFor="name-1">Quote</Label>
+              <Tiptap value={quote?.text} onValueUpdate={onValueUpdate} />
+            </div>
+          </div>
+                   <Separator className="my-0  bg-primary/10" />
+          
+          <TagField onChoose={onTagChoose} />
+          <div className="flex flex-wrap gap-2">
+            {
+              quote?.tags && quote?.tags.length > 0 && (
+                quote?.tags.map((tag) => {
+                  return (
+                    <Badge
+                      variant={'outline'}
+                      className=" bg-primary/10 border-primary/30 text-primary/90"
+                      key={tag.id}
+                    >
+                      #{tag.name}
+                    </Badge>
+                  )
+                }
+                )
+              )
+            }
+          </div>
+          
+          <Separator className="my-0 bg-primary/10" />
+          
+        
+      </motion.div>
+    </AnimatePresence>
+  </div>
+  )
+}

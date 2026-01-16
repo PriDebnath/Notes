1a43e74d9f0d6052c5836813c7c6d8ee61255574
Pritam <debnathpritam0802@gmail.com>
2026-01-16 19:44:34 +0530

    added types of bg in quote

--- frontend/src/model/quote.model.ts
+++ frontend/src/model/quote.model.ts
@@ -1,29 +1,35 @@
-export interface Quote{
+import type { TextureKey, Pri_set } from "@/hook/use-background.hook";
+
+export interface Quote {
     id?: number | undefined;
     text: string;
+    texture?: TextureKey;
+    pri_set?: Pri_set;
 }
 
-export interface QuoteDetails{
+export interface QuoteDetails {
     id?: number | undefined;
     text: string;
     tags?: Tag[]
 }
 
 
-export interface Tag{
+export interface Tag {
     id?: number | undefined;
     name: string;
 }
 
-export interface QuoteTags{
+export interface QuoteTags {
     id?: number | undefined;
     quoteId: number;
     tagId: number;
 }
 
 
-export interface QuoteFormData{
-  id?: number | undefined;
-  text?: string;
-  tags?: string[];
+export interface QuoteFormData {
+    id?: number | undefined;
+    text?: string;
+    tags?: string[];
+    texture?: TextureKey;
+    pri_set?: Pri_set;
 }

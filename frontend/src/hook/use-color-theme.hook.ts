d75ea97061d744e595f6a719f87bc83a439b151a
Pritam <debnathpritam0802@gmail.com>
2026-01-19 15:13:10 +0530

    Added selected color theme to local storage

--- frontend/src/hook/use-color-theme.hook.ts
+++ frontend/src/hook/use-color-theme.hook.ts
@@ -1,17 +1,21 @@
 import { useEffect, useState } from "react"
+import  { useLocalStorage } from "@/hook/use-localstroage.hook"
 
 export type ColorTheme = "theme-neutral" | "theme-rose"
 
 export const colorThemes: ColorTheme[] = ["theme-neutral", "theme-rose"]
 
 export const useColorTheme = () => {
-    const [colorTheme, setColorTheme] = useState<ColorTheme>("theme-rose")
+    const  [ stroredColorTheme, setStroredColorTheme ]= useLocalStorage( "app_theme", "theme-rose")
+    const [colorTheme, setColorTheme] = useState<ColorTheme>(stroredColorTheme as  ColorTheme)
+
     useEffect(() => {
         let root = document.documentElement
         for (let t of colorThemes) {
             root.classList.remove(t)
         }
         root.classList.add(colorTheme)
+        setStroredColorTheme(colorTheme)
     }, [colorTheme])
     return { colorTheme, setColorTheme }
 }
---
+++ frontend/src/hook/use-localstroage.hook.ts
@@ -0,0 +1,25 @@
+import { useState } from "react";
+export const STORAGE_KEYS = {
+  THEME: "app_theme",
+} as const;
+
+// union type of all values
+export type StorageKey = typeof STORAGE_KEYS[keyof typeof STORAGE_KEYS];
+
+
+export function useLocalStorage<T>(
+  key: StorageKey,
+  initialValue: T
+): [T, (value: T) => void] {
+  const [value, setValue] = useState<T>(() => {
+    const stored = localStorage.getItem(key);
+    return stored ? (JSON.parse(stored) as T) : initialValue;
+  });
+
+  const setStoredValue = (newValue: T) => {
+    setValue(newValue);
+    localStorage.setItem(key, JSON.stringify(newValue));
+  };
+
+  return [value, setStoredValue];
+}

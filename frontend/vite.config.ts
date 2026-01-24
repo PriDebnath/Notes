456d9fc291e2117b8141c6dc8b2eb4919b8ca899
Pritam <debnathpritam0802@gmail.com>
2026-01-24 10:52:00 +0530

    added PWA config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,9 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import routerPlugin  from '@tanstack/router-plugin/vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import tailwindcss from '@tailwindcss/vite'
+import routerPlugin from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,6 +18,38 @@ export default defineConfig({
       }
     ),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'Notes/logo/favicon-32x32.png',
+        'Notes/logo/android-chrome-192x192.png',
+        'Notes/logo/android-chrome-512x512.png'
+      ]
+      ,
+      manifest: {
+        name: 'Notes',
+        short_name: 'Notes',
+        description: 'Personal notes you want to revisit',
+        start_url: '/Notes/',
+        scope: '/Notes/',
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/Notes/logo/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/Notes/logo/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+
+      }
+    })
   ],
   resolve: {
     alias: {

ebb3d69349ba6b59ef16545073f894a538e3e498
Pritam <debnathpritam0802@gmail.com>
2026-01-24 11:44:42 +0530

    Ran " npx cap init"

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pritam.notes',
+  appName: 'Notes',
+  webDir: 'dist'
+};
+
+export default config;

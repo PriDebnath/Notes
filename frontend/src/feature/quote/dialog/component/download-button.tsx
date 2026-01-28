24fb5217dfdd7405c0430b8b72e5ad0e4ce34826
Pritam <debnathpritam0802@gmail.com>
2026-01-28 16:29:17 +0530

    moved download

--- frontend/src/feature/quote/dialog/component/download-button.tsx
+++ frontend/src/feature/quote/dialog/component/download-button.tsx
@@ -19,9 +19,28 @@ export const DownloadButton = (props: Props) => {
     const [downloadStatus, setDownloadStatus] = useState<Status>("idle")
 
     const handleDownload = async () => {
-        setDownloadStatus("pending")
-        await exportAsImage(elementRef.current, { backgroundColor: option.backgroundColor! })
-        setDownloadStatus("success")
+        if (!elementRef.current) return
+
+        try {
+            setDownloadStatus("pending")
+
+            const dataUrl = await exportAsImage(elementRef.current, {
+                backgroundColor: option.backgroundColor,
+            })
+            if (!dataUrl) {
+                console.warn('No dataUrl found')
+                return
+            }
+            const link = document.createElement("a")
+            const fileName = "note-by-pri-" + new Date().getTime() + "-.png"
+            link.download = fileName
+            link.href = dataUrl
+            link.click()
+            setDownloadStatus("success")
+        } catch (err) {
+            console.error("Download failed", err)
+            setDownloadStatus("idle")
+        }
 
         setTimeout(() => {
             setDownloadStatus("idle")

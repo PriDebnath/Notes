1f6789f8983a3989deca53edf9086c662e5238ca
Pritam <debnathpritam0802@gmail.com>
2026-02-13 15:11:17 +0530

    added first component testing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,47 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 import routerPlugin from '@tanstack/router-plugin/vite'
+import { VitePWA, type VitePWAOptions } from 'vite-plugin-pwa'
+
+
+const vitePWAOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+
+  includeAssets: [
+    'logo/favicon-32x32.png',
+    'logo/android-chrome-192x192.png',
+    'logo/android-chrome-512x512.png',
+  ],
+
+  manifest: {
+    name: 'Notes',
+    short_name: 'Notes',
+    description: 'Personal notes you want to revisit',
+
+    // 🔑 RELATIVE → works everywhere
+    start_url: '.',
+    scope: '.',
+
+    display: 'standalone',
+    theme_color: '#000000',
+    background_color: '#ffffff',
+
+    icons: [
+      {
+        src: 'logo/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'logo/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
+}
 
 export default defineConfig(({ mode }) => {
   const isGithub = mode === 'github'
@@ -13,54 +51,20 @@ export default defineConfig(({ mode }) => {
     // Android → "./"
     // GitHub Pages → "/Notes/"
     base: isGithub ? '/Notes/' : './',
-
+    test: {
+      globals: true, // Setting globals: true exposes the describe and it functions on the global object, so you don't need to import them in every test file. 
+      environment: 'jsdom',
+      // setupFiles: './src/test/setup.js',
+    },
     plugins: [
-      routerPlugin(),
-
       react({
         babel: {
           plugins: [['babel-plugin-react-compiler']],
         },
       }),
-
       tailwindcss(),
-
-      VitePWA({
-        registerType: 'autoUpdate',
-
-        includeAssets: [
-          'logo/favicon-32x32.png',
-          'logo/android-chrome-192x192.png',
-          'logo/android-chrome-512x512.png',
-        ],
-
-        manifest: {
-          name: 'Notes',
-          short_name: 'Notes',
-          description: 'Personal notes you want to revisit',
-
-          // 🔑 RELATIVE → works everywhere
-          start_url: '.',
-          scope: '.',
-
-          display: 'standalone',
-          theme_color: '#000000',
-          background_color: '#ffffff',
-
-          icons: [
-            {
-              src: 'logo/android-chrome-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: 'logo/android-chrome-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-            },
-          ],
-        },
-      }),
+      routerPlugin(),
+      VitePWA(vitePWAOptions),
     ],
 
     resolve: {

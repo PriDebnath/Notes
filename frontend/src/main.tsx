085b8546b82cd8d7def3d73f13bed861cbe3284a
Pritam <debnathpritam0802@gmail.com>
2026-03-19 15:23:09 +0530

    Ran "pnpm dlx shadcn@latest add sonner" to install toaster

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,10 +3,13 @@ import './tiptap.scss'
 // import App from './App.tsx'
 import { StrictMode, type JSX } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster } from "@/components/ui/sonner"
+
 import { MainProvider } from '@/provider/main.provider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MainProvider />
+    <Toaster />
   </StrictMode>,
 )

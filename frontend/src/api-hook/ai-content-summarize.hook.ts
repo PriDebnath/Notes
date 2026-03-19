21479883495a741e7b2699f0a2192b84b89f4f2e
Pritam <debnathpritam0802@gmail.com>
2026-03-19 16:18:18 +0530

    created a hook  to get summary from note

---
+++ frontend/src/api-hook/ai-content-summarize.hook.ts
@@ -0,0 +1,35 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+
+const VITE_GROQ_API_KEY =  import.meta.env.VITE_GROQ_API_KEY
+
+if (!VITE_GROQ_API_KEY) {
+    console.warn("GROQ API KEY not found, ai powered features may not work")
+}
+
+const getSummarize = async (content: string) => {
+      const prompt = `Summarize this note:\n${content}`;
+
+    const response = await fetch("https://api.groq.com/openai/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${VITE_GROQ_API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "llama-3.1-8b-instant",
+            max_completion_tokens: 200,
+            messages: [{ role: "user", content: prompt }]
+        })
+    });
+    return await response.json()
+}
+
+export const useGetSummarize = () => {
+    const mutation = useMutation({
+        mutationFn: getSummarize
+    })
+    return {
+        ...mutation,
+        getSummarize: mutation.mutateAsync,
+    }
+}

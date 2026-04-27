f4848fecd3a1f802567bcefecd4ad4e2e00f6731
Pritam <debnathpritam0802@gmail.com>
2026-04-27 13:50:31 +0530

    added load env

--- backend-express/src/server.ts
+++ backend-express/src/server.ts
@@ -1,4 +1,14 @@
 import {app} from "./app"
+import {  getEnv, loadEnv } from "./utils/load-env";
 
+async function startServer() {
+  await loadEnv();
 
-app.
+  const { PORT } = getEnv();
+
+  app.listen(PORT || 8000, () => {
+    console.log(`🟩 Server running on port ${PORT}`);
+  });
+}
+
+startServer();
---
+++ backend-express/src/utils/load-env.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+import path from "path";
+import dotenv from "dotenv";
+
+const envSchema = z.object({
+    MONGO_URI: z.string(),
+    PORT: z.coerce.number(),
+});
+
+type Env = z.infer<typeof envSchema>;
+
+let env: Env;
+
+export const loadEnv = async () => {
+    dotenv.config({
+         path: path.resolve(__dirname, "../../.env"), 
+        });
+
+    try {
+        env = await envSchema.parseAsync(process.env);
+        console.log("🟩 env data loaded");
+    } catch (error) {
+        console.error("🟥 could not load env data");
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+export const getEnv = (): Env => {
+    if (!env) {
+        throw new Error("Env not loaded. Call loadEnv() first.");
+    }
+    return env;
+};
